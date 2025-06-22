6238111488aa5d3b788813b3638ff813102a7011
squirrelsheregesh <dlebedev@psystems.biz>
2025-06-23 02:58:46 +0300

    feat(ci): fix vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,19 @@
+/*
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
     outDir: '/app/dist',
+    emptyOutDir: true
+  }
+});
+
+*/
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist', // Раньше было: /app/dist — теперь локально в frontend/dist
     emptyOutDir: true
   }
 });
