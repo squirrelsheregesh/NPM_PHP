2b152787eb3d02d19c10f73933a133f887f55a89
squirrelsheregesh <dlebedev@psystems.biz>
2025-06-23 02:44:52 +0300

    feat(front,back): add main meat

---
+++ backend/index.php
@@ -0,0 +1,2 @@
+<?php
+echo "<h1>Hello from PHP Backend</h1>";
---
+++ frontend/vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    outDir: '/app/dist',
+    emptyOutDir: true
+  }
+});
+
